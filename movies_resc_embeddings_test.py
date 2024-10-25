a727e3ab3e5b801cb1abeff28753f7d9097469df
Sahitya Satya <96128126+sahitya12345@users.noreply.github.com>
2024-10-25 21:39:49 +0530

    Add files via upload

---
+++ movies_resc_embeddings_test.py
@@ -0,0 +1,37 @@
+import pymongo
+import requests
+
+client = pymongo.MongoClient("mongodb+srv://sahityasaty16:Z6pKlZEVDnyB3r89@cluster0.tploxtx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client.sample_mflix
+collection = db.movies
+
+hf_token = "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"
+embedding_url = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
+
+query = "war"
+
+def generate_embedding(text: str) -> list[float]:
+
+  response = requests.post(
+    embedding_url,
+    headers={"Authorization": f"Bearer {hf_token}"},
+    json={"inputs": text})
+
+  if response.status_code != 200:
+    raise ValueError(f"Request failed with status code {response.status_code}: {response.text}")
+
+  return response.json()
+
+results = collection.aggregate([
+  {"$vectorSearch": {
+    "queryVector": generate_embedding(query),
+    "path": "plot_embedding_hf",
+    "numCandidates": 100,
+    "limit": 4,
+    "index": "PlotSemanticSearch",
+      }}
+]);
+
+print(f"Query: {query}")
+for document in results:
+    print(f'Movie Name: {document["title"]},\nMovie Plot: {document["plot"]}\n')
